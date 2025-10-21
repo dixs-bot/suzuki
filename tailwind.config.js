9ba1d6bf71f456664c37c93eb255e8fd4d5df071
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2025-10-21 23:08:34 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        suzukiBlue: '#0033A0',
+        suzukiRed: '#E60012'
+      }
+    }
+  },
+  plugins: []
+}
